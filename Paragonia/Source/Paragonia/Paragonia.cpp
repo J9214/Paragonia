71ab4f96ce3a5c25708b9474df0e5058636e3cad
KiSungKim23 <essuttr3@gmail.com>
2025-12-08 18:07:13 +0900

    FirstAsset

---
+++ Paragonia/Source/Paragonia/Paragonia.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Paragonia.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Paragonia, "Paragonia" );
