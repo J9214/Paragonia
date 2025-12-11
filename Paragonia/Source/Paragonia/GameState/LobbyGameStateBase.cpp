52aac98a18bdc7c2c09e4a4a5c6928aca390e6b4
Hyun <hnjogo@gmail.com>
2025-12-11 15:30:37 +0900

    [Add] Lobby GameState

--- Paragonia/Source/Paragonia/GameState/LobbyGameStateBase.cpp
+++ Paragonia/Source/Paragonia/GameState/LobbyGameStateBase.cpp
@@ -2,4 +2,52 @@
 
 
 #include "GameState/LobbyGameStateBase.h"
+#include "Net/UnrealNetwork.h"
 
+ALobbyGameStateBase::ALobbyGameStateBase()
+	:LeftTime(0)
+{
+	CurrentLobbyState = EGameLobbyState::GLS_WaitingForPlayers;
+}
+
+void ALobbyGameStateBase::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(ThisClass, CurrentLobbyState);
+	DOREPLIFETIME(ThisClass, LeftTime);
+}
+
+void ALobbyGameStateBase::SetLobbyState(EGameLobbyState NewState)
+{
+	if (HasAuthority())
+	{
+		CurrentLobbyState = NewState;
+		OnRep_CurrentLobbyState();
+	}
+}
+
+void ALobbyGameStateBase::SetLeftTime(int32 NewTime)
+{
+	if (HasAuthority())
+	{
+		LeftTime = NewTime;
+		OnRep_LeftTime();
+	}
+}
+
+void ALobbyGameStateBase::OnRep_CurrentLobbyState()
+{
+	if (OnLobbyGameStateChanged.IsBound())
+	{
+		OnLobbyGameStateChanged.Broadcast(CurrentLobbyState);
+	}
+}
+
+void ALobbyGameStateBase::OnRep_LeftTime()
+{
+	if (OnLeftTimeChanged.IsBound())
+	{
+		OnLeftTimeChanged.Broadcast(LeftTime);
+	}
+}
--- Paragonia/Source/Paragonia/GameState/LobbyGameStateBase.h
+++ Paragonia/Source/Paragonia/GameState/LobbyGameStateBase.h
@@ -6,6 +6,17 @@
 #include "GameFramework/GameStateBase.h"
 #include "LobbyGameStateBase.generated.h"
 
+UENUM(BlueprintType)
+enum class EGameLobbyState : uint8
+{
+    GLS_WaitingForPlayers,
+    GLS_CharacterSelect,
+    GLS_GameStarting,
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnLobbyGameStateChangedDelegate, EGameLobbyState, NewState);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnLeftTimeChangedDelegate, int32, NewTime);
+
 /**
  * 
  */
@@ -13,5 +24,40 @@ UCLASS()
 class PARAGONIA_API ALobbyGameStateBase : public AGameStateBase
 {
 	GENERATED_BODY()
-	
+public:
+	ALobbyGameStateBase();
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "LobbyState")
+	EGameLobbyState GetCurrentLobbyState() const { return CurrentLobbyState; }
+
+	UFUNCTION(BlueprintCallable, Category = "LobbyState")
+	int32 GetLeftTime() const { return LeftTime; }
+
+	// Only Server Set
+	void SetLobbyState(EGameLobbyState NewState);
+	void SetLeftTime(int32 NewTime);
+
+public:
+	UPROPERTY(BlueprintAssignable, Category = "Events")
+	FOnLobbyGameStateChangedDelegate OnLobbyGameStateChanged;
+
+	UPROPERTY(BlueprintAssignable, Category = "Events")
+	FOnLeftTimeChangedDelegate OnLeftTimeChanged;
+
+protected:
+	UFUNCTION()
+	void OnRep_CurrentLobbyState();
+
+	UFUNCTION()
+	void OnRep_LeftTime();
+
+protected:
+	UPROPERTY(ReplicatedUsing = OnRep_CurrentLobbyState)
+	EGameLobbyState CurrentLobbyState;
+
+	UPROPERTY(ReplicatedUsing = OnRep_LeftTime)
+	int32 LeftTime;
 };
