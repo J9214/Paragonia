598acfdcc7163d889603ae83cea4650e50a44e73
Hyun <hnjogo@gmail.com>
2025-12-11 14:36:30 +0900

    [Add] Lobby Player State
    - 상태 Enum 정의 및 필요 변수 Replicate
    - OnRep 함수와 RPC 함수 추가
    - 관련 변수 Delegate

--- Paragonia/Source/Paragonia/PlayerState/LobbyPlayerState.cpp
+++ Paragonia/Source/Paragonia/PlayerState/LobbyPlayerState.cpp
@@ -2,4 +2,100 @@
 
 
 #include "PlayerState/LobbyPlayerState.h"
+#include "Net/UnrealNetwork.h"
+#include "PlayerState/PGPlayerState.h"
 
+void ALobbyPlayerState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(ThisClass, PlayerLobbyState);
+	DOREPLIFETIME(ThisClass, CharacterID);
+	DOREPLIFETIME(ThisClass, TeamID);
+}
+
+void ALobbyPlayerState::CopyProperties(APlayerState* PlayerState)
+{
+	Super::CopyProperties(PlayerState);
+
+	APGPlayerState* TargetPS = Cast<APGPlayerState>(PlayerState);
+	if (TargetPS)
+	{
+		// 차후 수정 예정
+	}
+}
+
+void ALobbyPlayerState::ServerSetLobbyState_Implementation(EPlayerLobbyState NewState)
+{
+	PlayerLobbyState = NewState;
+
+	OnRep_PlayerLobbyState();
+}
+
+bool ALobbyPlayerState::ServerSetLobbyState_Validate(EPlayerLobbyState NewState)
+{
+	if(NewState == PlayerLobbyState)
+		return false;
+
+	return true;
+}
+
+void ALobbyPlayerState::ServerSetCharacterID_Implementation(int32 NewID)
+{
+	CharacterID = NewID;
+
+	OnRep_CharacterID();
+}
+
+bool ALobbyPlayerState::ServerSetCharacterID_Validate(int32 NewID)
+{
+	if (NewID < 0)
+		return false;
+
+	if (NewID == CharacterID)
+		return false;
+
+	return true;
+}
+
+void ALobbyPlayerState::ServerSetTeamID_Implementation(int32 NewTeamID)
+{
+	TeamID = NewTeamID;
+
+	OnRep_TeamID();
+}
+
+bool ALobbyPlayerState::ServerSetTeamID_Validate(int32 NewTeamID)
+{
+	if (NewTeamID < 0)
+		return false;
+
+	if (NewTeamID == TeamID)
+		return false;
+
+	return true;
+}
+
+void ALobbyPlayerState::OnRep_PlayerLobbyState()
+{
+	if (OnLobbyStateChanged.IsBound())
+	{
+		OnLobbyStateChanged.Broadcast(PlayerLobbyState);
+	}
+}
+
+void ALobbyPlayerState::OnRep_CharacterID()
+{
+	if (OnCharacterIDChanged.IsBound())
+	{
+		OnCharacterIDChanged.Broadcast(CharacterID);
+	}
+}
+
+void ALobbyPlayerState::OnRep_TeamID()
+{
+	if (OnTeamIDChanged.IsBound())
+	{
+		OnTeamIDChanged.Broadcast(TeamID);
+	}
+}
--- Paragonia/Source/Paragonia/PlayerState/LobbyPlayerState.h
+++ Paragonia/Source/Paragonia/PlayerState/LobbyPlayerState.h
@@ -6,12 +6,80 @@
 #include "GameFramework/PlayerState.h"
 #include "LobbyPlayerState.generated.h"
 
+UENUM(BlueprintType)
+enum class EPlayerLobbyState : uint8
+{
+	PLS_NotReady,
+	PLS_MatchingReady,
+	PLS_Selecting,
+	PLS_SelectedAndReady,
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnLobbyStateChangedDelegate, EPlayerLobbyState, NewState);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnCharacterIDChangedDelegate, int32, NewCharacterID);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnTeamIDChangedDelegate, int32, NewTeamID);
+
 /**
- * 
+ *
  */
 UCLASS()
 class PARAGONIA_API ALobbyPlayerState : public APlayerState
 {
 	GENERATED_BODY()
-	
+
+public:
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	virtual void CopyProperties(APlayerState* PlayerState) override;
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "LobbyData")
+	EPlayerLobbyState GetPlayerLobbyState() const { return PlayerLobbyState; }
+
+	UFUNCTION(BlueprintCallable, Category = "LobbyData")
+	int32 GetCharacterID() const { return CharacterID; }
+
+	UFUNCTION(BlueprintCallable, Category = "LobbyData")
+	int32 GetTeamID() const { return TeamID; }
+
+
+	// Client -> Server RPC
+	UFUNCTION(Server, Reliable, WithValidation)
+	void ServerSetLobbyState(EPlayerLobbyState NewState);
+
+	UFUNCTION(Server, Reliable, WithValidation)
+	void ServerSetCharacterID(int32 NewID);
+
+	UFUNCTION(Server, Reliable, WithValidation)
+	void ServerSetTeamID(int32 NewTeamID);
+
+public:
+	// Delegates
+	UPROPERTY(BlueprintAssignable, Category = "Events")
+	FOnLobbyStateChangedDelegate OnLobbyStateChanged;
+
+	UPROPERTY(BlueprintAssignable, Category = "Events")
+	FOnCharacterIDChangedDelegate OnCharacterIDChanged;
+
+	UPROPERTY(BlueprintAssignable, Category = "Events")
+	FOnTeamIDChangedDelegate OnTeamIDChanged;
+protected:
+	UFUNCTION()
+	void OnRep_PlayerLobbyState();
+
+	UFUNCTION()
+	void OnRep_CharacterID();
+
+	UFUNCTION()
+	void OnRep_TeamID();
+
+protected:
+	UPROPERTY(ReplicatedUsing = OnRep_PlayerLobbyState)
+	EPlayerLobbyState PlayerLobbyState;
+
+	UPROPERTY(ReplicatedUsing = OnRep_CharacterID)
+	int32 CharacterID;
+
+	UPROPERTY(ReplicatedUsing = OnRep_TeamID)
+	int32 TeamID;
 };
