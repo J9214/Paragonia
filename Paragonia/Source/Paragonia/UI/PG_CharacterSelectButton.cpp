75c9abb706e4f191e7f9e6785e0daf7899637ce6
KiSungKim23 <essuttr3@gmail.com>
2025-12-11 20:56:36 +0900

    [Add] Character Select UI 이미지만 추가

---
+++ Paragonia/Source/Paragonia/UI/PG_CharacterSelectButton.cpp
@@ -0,0 +1,8 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/PG_CharacterSelectButton.h"
+
+void UPG_CharacterSelectButton::NativeOnListItemObjectSet(UObject* ListItemObject)
+{
+}
---
+++ Paragonia/Source/Paragonia/UI/PG_CharacterSelectButton.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonButtonBase.h"
+#include "Blueprint/IUserObjectListEntry.h"
+#include "PG_CharacterSelectButton.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PARAGONIA_API UPG_CharacterSelectButton : public UCommonButtonBase, public IUserObjectListEntry
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeOnListItemObjectSet(UObject* ListItemObject) override;
+	
+};
