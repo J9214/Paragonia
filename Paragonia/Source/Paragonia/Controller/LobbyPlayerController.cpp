f8011214ce54942370a2a04d96c43f44a9d8a56a
Hyun <hnjogo@gmail.com>
2025-12-12 15:37:00 +0900

    [Add] Lobby PC - Create UI

--- Paragonia/Source/Paragonia/Controller/LobbyPlayerController.cpp
+++ Paragonia/Source/Paragonia/Controller/LobbyPlayerController.cpp
@@ -2,4 +2,71 @@
 
 
 #include "Controller/LobbyPlayerController.h"
+#include "Blueprint/UserWidget.h"
+#include "UI/PG_LobbyWidget.h"
+#include "UI/PG_CharacterSelectWidget.h"
 
+void ALobbyPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (IsLocalController() == false)
+	{
+		return;
+	}
+
+    if (MatchStartUIWidgetClass)
+    {
+        MatchStartUIWidgetInstance = CreateWidget<UPG_LobbyWidget>(this, MatchStartUIWidgetClass);
+        if (MatchStartUIWidgetInstance)
+        {
+            MatchStartUIWidgetInstance->AddToViewport();
+            MatchStartUIWidgetInstance->SetVisibility(ESlateVisibility::Visible);
+        }
+    }
+
+    if (CharSelectUIWidgetClass)
+    {
+        CharSelectUIWidgetInstance = CreateWidget<UPG_CharacterSelectWidget>(this, CharSelectUIWidgetClass);
+        if (CharSelectUIWidgetInstance)
+        {
+            CharSelectUIWidgetInstance->AddToViewport();
+            CharSelectUIWidgetInstance->SetVisibility(ESlateVisibility::Collapsed);
+        }
+    }
+
+    bShowMouseCursor = true;
+    FInputModeUIOnly InputMode;
+    SetInputMode(InputMode);
+
+	// delegate 등록하기
+
+}
+
+void ALobbyPlayerController::ShowCharacterSelect()
+{
+    if (MatchStartUIWidgetInstance)
+    {
+        MatchStartUIWidgetInstance->SetVisibility(ESlateVisibility::Collapsed);
+    }
+
+    if (CharSelectUIWidgetInstance)
+    {
+        CharSelectUIWidgetInstance->SetVisibility(ESlateVisibility::Visible);
+
+        // CharSelectUIWidgetInstance->Init(); 
+    }
+}
+
+void ALobbyPlayerController::ShowMatchStart()
+{
+    if (CharSelectUIWidgetInstance)
+    {
+        CharSelectUIWidgetInstance->SetVisibility(ESlateVisibility::Collapsed);
+    }
+
+    if (MatchStartUIWidgetInstance)
+    {
+        MatchStartUIWidgetInstance->SetVisibility(ESlateVisibility::Visible);
+    }
+}
--- Paragonia/Source/Paragonia/Controller/LobbyPlayerController.h
+++ Paragonia/Source/Paragonia/Controller/LobbyPlayerController.h
@@ -6,12 +6,37 @@
 #include "GameFramework/PlayerController.h"
 #include "LobbyPlayerController.generated.h"
 
+class UPG_LobbyWidget;
+class UPG_CharacterSelectWidget;
+
+
 /**
- * 
+ *
  */
 UCLASS()
 class PARAGONIA_API ALobbyPlayerController : public APlayerController
 {
 	GENERATED_BODY()
-	
+public:
+	virtual void BeginPlay() override;
+
+protected:
+	UFUNCTION(Category = UI)
+	void ShowCharacterSelect();
+
+	UFUNCTION(Category = UI)
+	void ShowMatchStart();
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = UI, Meta = (AllowPrivateAccess))
+	TSubclassOf<UPG_LobbyWidget> MatchStartUIWidgetClass;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = UI, Meta = (AllowPrivateAccess))
+	TObjectPtr<UPG_LobbyWidget> MatchStartUIWidgetInstance;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = UI, Meta = (AllowPrivateAccess))
+	TSubclassOf<UPG_CharacterSelectWidget> CharSelectUIWidgetClass;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = UI, Meta = (AllowPrivateAccess))
+	TObjectPtr<UPG_CharacterSelectWidget> CharSelectUIWidgetInstance;
 };
