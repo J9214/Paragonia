e34c4397690fe9af1b3369f5a0cbc17e17af868c
Hyun <hnjogo@gmail.com>
2025-12-18 17:44:32 +0900

    [Add] MinionCharacter Class

---
+++ Paragonia/Source/Paragonia/Character/AI/MinionCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/AI/NpcBaseCharacter.h"
+#include "MinionCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PARAGONIA_API AMinionCharacter : public ANpcBaseCharacter
+{
+	GENERATED_BODY()
+	
+};
