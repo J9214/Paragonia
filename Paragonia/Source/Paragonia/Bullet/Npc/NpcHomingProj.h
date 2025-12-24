bb01aa164a38523b56ab0904decf7f659b362004
Hyun <hnjogo@gmail.com>
2025-12-24 16:20:35 +0900

    [Add] Minion 공격용 투사체 Actor

---
+++ Paragonia/Source/Paragonia/Bullet/Npc/NpcHomingProj.h
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "GameplayEffectTypes.h"
+#include "NpcHomingProj.generated.h"
+
+class USphereComponent;
+class UProjectileMovementComponent;
+
+UCLASS()
+class PARAGONIA_API ANpcHomingProj : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ANpcHomingProj();
+
+	void InitializeProjectile(AActor* Target, float Speed);
+	void DeactivateProjectile();
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+protected:
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnRep_HomingTarget();
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	TObjectPtr<USphereComponent> SphereComp;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	TObjectPtr<UStaticMeshComponent> MeshComp;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	TObjectPtr<UProjectileMovementComponent> MovementComp;
+
+	UPROPERTY(EditDefaultsOnly, Category = "LifeTime")
+	float MaxLifeTime;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "GAS")
+	FGameplayTag HitEventTag;
+
+	UPROPERTY(ReplicatedUsing = OnRep_HomingTarget)
+	AActor* HomingTargetActor;
+};
