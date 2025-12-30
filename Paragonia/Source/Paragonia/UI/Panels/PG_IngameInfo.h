dfea415b109f5b108862bf4e7f5785cdda645c81
KiSungKim23 <essuttr3@gmail.com>
2025-12-30 20:53:21 +0900

    [Fix] TestMap 삭제 안쓰이는 코드 주석처리 게임시작인원4인 세팅

--- Paragonia/Source/Paragonia/UI/Panels/PG_IngameInfo.h
+++ Paragonia/Source/Paragonia/UI/Panels/PG_IngameInfo.h
@@ -47,23 +47,23 @@ protected:
 	UPROPERTY(meta = (BindWidget))
 	TObjectPtr<UTextBlock> PlayerName;
 
-	UPROPERTY(meta = (BindWidget))
-	TObjectPtr<UPGInventorySlotWidget> Item0;
+	//UPROPERTY(meta = (BindWidget))
+	//TObjectPtr<UPGInventorySlotWidget> Item0;
 
-	UPROPERTY(meta = (BindWidget))
-	TObjectPtr<UPGInventorySlotWidget> Item1;
+	//UPROPERTY(meta = (BindWidget))
+	//TObjectPtr<UPGInventorySlotWidget> Item1;
 
-	UPROPERTY(meta = (BindWidget))
-	TObjectPtr<UPGInventorySlotWidget> Item2;
+	//UPROPERTY(meta = (BindWidget))
+	//TObjectPtr<UPGInventorySlotWidget> Item2;
 
-	UPROPERTY(meta = (BindWidget))
-	TObjectPtr<UPGInventorySlotWidget> Item3;
+	//UPROPERTY(meta = (BindWidget))
+	//TObjectPtr<UPGInventorySlotWidget> Item3;
 
-	UPROPERTY(meta = (BindWidget))
-	TObjectPtr<UPGInventorySlotWidget> Item4;
+	//UPROPERTY(meta = (BindWidget))
+	//TObjectPtr<UPGInventorySlotWidget> Item4;
 
-	UPROPERTY(meta = (BindWidget))
-	TObjectPtr<UPGInventorySlotWidget> Item5;
+	//UPROPERTY(meta = (BindWidget))
+	//TObjectPtr<UPGInventorySlotWidget> Item5;
 private:
 	UPROPERTY()
 	TObjectPtr<UCharacterAttributeSet> BoundAttrSet;
