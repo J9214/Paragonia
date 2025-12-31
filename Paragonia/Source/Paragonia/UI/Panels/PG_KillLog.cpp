608995760b857c95b7dff8050a195d064d52ea69
KiSungKim23 <essuttr3@gmail.com>
2025-12-31 14:07:16 +0900

    [Fix] 킬로그 작업 출력확인 필요

---
+++ Paragonia/Source/Paragonia/UI/Panels/PG_KillLog.cpp
@@ -0,0 +1,84 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/Panels/PG_KillLog.h"
+#include "Components/TextBlock.h"
+#include "UI/Icons/PG_PlayerIcon.h"
+#include "PlayerState/PGPlayerState.h"
+#include "Components/Image.h"
+
+void UPG_KillLog::ResetSlot()
+{
+    bInited = false;
+    BoundPlayerId = INDEX_NONE;
+
+    if (KillerIcon)
+    {
+        KillerIcon->SetVisibility(ESlateVisibility::Hidden);
+    }
+    if (LogLabel)
+    {
+        LogLabel->SetText(FText::GetEmpty());
+    }
+
+    StopAnimation(SetInit);
+    PlayAnimation(SetInit, 0.f, 1, EUMGSequencePlayMode::Forward, 1.f);
+}
+void UPG_KillLog::InitIfNeeded(APGPlayerState* InPlayerState, APGPlayerState* DeathPlayerState)
+{
+    if (!IsValid(InPlayerState) || !IsValid(DeathPlayerState))
+    {
+        UE_LOG(LogTemp, Warning, TEXT("Return"));
+        return;
+    }
+
+    UE_LOG(LogTemp, Warning, TEXT("INIT"));
+    const int32 PlayerId = DeathPlayerState->GetPlayerId();
+    if (bInited && BoundPlayerId == PlayerId)
+    {
+        return;
+    }
+
+    if (InPlayerState->GetTeamID() != DeathPlayerState->GetTeamID())
+    {
+        if (IsValid(TeamImage))
+        {
+            TeamImage->SetVisibility(ESlateVisibility::Hidden);
+        }
+    }
+
+    if (VictimIcon)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("SetPlayerIcon"));
+        VictimIcon->SetPlayerIcon(DeathPlayerState->GetCharID());
+    }
+
+    BoundPlayerId = PlayerId;
+    bInited = true;
+}
+
+void UPG_KillLog::ShowKillLog(APGPlayerState* KillerPlayerState)
+{
+    if (KillerIcon)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("ShowLog"));
+        if (KillerPlayerState == nullptr)
+        {
+            KillerIcon->SetVisibility(ESlateVisibility::Hidden);
+        }
+        else
+        {
+            KillerIcon->SetVisibility(ESlateVisibility::Visible);
+            KillerIcon->SetPlayerIcon(KillerPlayerState->GetCharID());
+        }
+    }
+
+    if (PulseAnim)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("PlayAnim"));
+        this->SetVisibility(ESlateVisibility::Visible);
+        StopAnimation(PulseAnim);
+        PlayAnimation(PulseAnim, 0.f, 1, EUMGSequencePlayMode::Forward, 1.f);
+    }
+
+}
